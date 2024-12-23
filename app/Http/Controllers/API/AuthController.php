8b8207e5017f721165ae0a9d05f5ff0b428f85e6
oogke <actlikeanut@gmail.com>
2024-12-23 09:47:48 +0545

    register user

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -3,9 +3,61 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
-    //
+    public function signup(Request $request)
+    {
+$validateData=Validator::make(
+    $request->all(),[
+        'firstname' => 'required|alpha',
+        'lastname' => 'required|alpha',
+        'email' => 'required|email|unique:users,email',
+        'password' => 'required|min:6'
+
+    ]);
+    if($validateData->fails())
+    {
+        return response()->json([
+   'status' => false,
+   'message' => 'Validation Error',
+   'errors' => $validateData->errors()->all()
+        ],405);
+    }
+$user =User::create([
+'firstname'=>$request->firstname,
+'lastname'=>$request->lastname,
+'email'=>$request->email,
+'password'=>$request->password
+]);
+
+return response()->json([
+'status'=> true,
+'message'=>'user registered successfully',
+'user' => $user
+],200);
+
+    }
+
+
+    // public function login(Request $request)
+    // {
+
+
+
+
+    // }
+
+
+
+    // public function logout(Request $request)
+    // {
+
+
+
+
+    // }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\API\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/register',[AuthController::class,'signup'])->name('register');
+Route::post('/login',[AuthController::class,'login'])->name('login');
+
+Route::middleware('auth:sanctum')->group(function()
+{
+Route::get('/logout',[AuthController::class,'logout'])->name('logout');
+});
