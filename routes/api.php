a72fcf18fab07fb521e7d24278e3680e52b4e521
oogke <actlikeanut@gmail.com>
2024-12-25 07:42:28 +0545

    Dec 25

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,15 @@
 <?php
 
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\CafeController;
 use App\Http\Controllers\API\CategoryController;
 use App\Http\Controllers\API\DistrictController;
+use App\Http\Controllers\API\EventController;
+use App\Http\Controllers\API\GuideController;
+use App\Http\Controllers\API\HomestayController;
+use App\Http\Controllers\API\HotelController;
 use App\Http\Controllers\API\PlaceController;
+use App\Http\Controllers\API\RestaurantController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,4 +23,11 @@ Route::get('/districts/{districtName}',[DistrictController::class,'show']);
 Route::apiResource('/districts',DistrictController::class)->except(['show']);
 Route::apiResource('/category',CategoryController::class)->except(['show']);
 Route::apiResource('/places',PlaceController::class)->except(['show']);
+Route::apiResource('/cafes',CafeController::class)->except(['show']);
+Route::apiResource('/restaurants',RestaurantController::class)->except(['show']);
+Route::apiResource('/homestays',HomestayController::class)->except(['show']);
+Route::apiResource('/hotels',HotelController::class)->except(['show']);
+Route::apiResource('/events',EventController::class)->except(['show']);
+Route::apiResource('/guides',GuideController::class)->except(['show']);
+
 });
